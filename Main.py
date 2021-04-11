90271f0c990c0b5f4601a2777f6ac5d57b980259
JoseDeJesusMendozaBecerril <pepemendoza1311@gmail.com>
2021-04-10 21:15:54 -0500

    Finalizado Analisis Exploratorio

---
+++ Main.py
@@ -0,0 +1,16 @@
+
+
+
+def main():
+    print("Hola Mundo")
+
+
+    #Limpieza de datos
+
+    # a. En caso de haber datos faltantes, reparar los valores empleando la media para el atributo en cuestión. 
+
+    
+
+
+
+main()
