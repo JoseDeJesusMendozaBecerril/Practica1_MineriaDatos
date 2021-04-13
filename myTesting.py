3f7d073b0cbb94c0ff868b185e00db403b80619c
clanersitow <45682025+clanersitow@users.noreply.github.com>
2021-04-12 19:52:14 -0500

    E2 almost ready

--- myTesting.py
+++ myTesting.py
@@ -18,64 +18,50 @@ filename2 = 'mtcars.txt'
 
 
 def main():
-    print("Hola Mundo")
+
     #Lectura de datos mtCars
+
     mtCars = pd.read_csv(filename2,sep=" ",header=0)
     mtCars = mtCars[['disp','hp','wt']]
     df=pd.DataFrame(mtCars)
 
+    #Lectura de datos variables de entrada
     disp = df['disp']
-    hp = df['hp']
     wt = df['wt']
-    
     X=np.array([disp,wt]).T
+
+    #Lectura de datos variables de salida
+    hp = df['hp']
     Y=np.array(hp).T
 
-    print(Y)
+    
 
     kf=KFold(n_splits=2,shuffle=True,random_state=2)
     regr=linear_model.LinearRegression()
 
     for values_x,values_y in kf.split(X):
-        #print(df.iloc[values_x],df.iloc[values_y])
-        #print(values_x,values_y)
+
         X_train,X_test = X[values_x],X[values_y]
         Y_train,Y_test = Y[values_x],Y[values_y]
-        #X_train,X_test = X_train.reshape(-1,1),X_test.reshape(-1,1)
+
         Y_train,Y_test = Y_train.reshape(-1,1),Y_test.reshape(-1,1)
         regr.fit(X_train,Y_train)
         y_pred=regr.predict(X_test)
 
         print(y_pred,Y_test)
+        print(regr.score(X_train,Y_train) )
         print(mean_squared_error(Y_test,y_pred) )
 
+        xx_pred, yy_pred = np.meshgrid(X_test[:,0], X_test[:,1])
+        model_viz = np.array([xx_pred.flatten(), yy_pred.flatten()]).T
+        predicted = regr.predict(model_viz)
+
+        fig =plt.figure()
+        ax= fig.add_subplot(111,projection='3d')
+        ax.scatter(X_test[:,0],X_test[:,1],Y_test[:,0],c='red',marker='o',alpha=0.5 )
+        ax.scatter(xx_pred.flatten(), yy_pred.flatten(), predicted, facecolor=(0,0,0,0),marker='o', s=25, edgecolor='#70b3f0')
+
+        plt.show()
+
     
-
-
-
-
-    """ mtCars = mtCars[['disp','hp','wt']]
-    
-    print(mtCars.shape)
-    print (mtCars.head(5))
-
-    disp = mtCars['disp'].values
-    hp = mtCars['hp'].values
-    wt = mtCars['wt'].values
-    
-    X=np.array([disp,wt]).T
-    Y=np.array(hp)
-
-    reg = linear_model.LinearRegression()
-    reg=reg.fit(X,Y)
-    Y_pred=reg.predict(X)
-
-    error = np.sqrt(mean_squared_error(Y,Y_pred))
-    r2=reg.score(X,Y)
-
-    print("error :",error)
-    print("r cuadrada", r2)
-    print("los coeficientes son: \n",reg.coef_)
-
- """
 main()
